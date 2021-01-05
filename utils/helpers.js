84bfed43fefc2b0fc4e1f2ea3e6410427a55714e
anovobilskischool <myemail>
2021-01-04 17:29:37 -0800

    stable without comment functionality

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,11 +9,11 @@ module.exports = {
 
     // Return a random emoji
     if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
+      return `( ಠ ͜ʖ ಠ)		`;
     } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
+      return `٩(◕‿◕)۶      `;
     } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
+      return `ଘ(੭ˊ꒳​ˋ)੭✧	`;
     }
   },
 };
